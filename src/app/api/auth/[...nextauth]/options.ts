9266d3ee6418100d4f1a5cd82a188db509f6d83c
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-05-12 18:52:54 +0530

    next auth implimentation

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,24 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { connectDB } from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+
+export const authOptions: NextAuthOptions ={
+    providers:[
+        CredentialsProvider({
+            id: "credentials",
+            name: "credentials",
+            
+            credentials:{
+               username: {label: "Username", type: "text", placeholder: "jsmith"},
+               password: {label: "Password", type: "password"}     
+            },
+
+            async authorize(credentials: any):Promise<any>{
+                
+            }
+
+        })
+    ]
+}
